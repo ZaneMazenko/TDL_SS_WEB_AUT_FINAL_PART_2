3753aaef0eafeee6024bd54e7c72d81ab9237047
Zane.Zvirgzdina <zane.zvirgzdina@yahoo.con>
2022-08-25 15:51:11 +0300

    Finished 1st scenario, partly done 2nd for Final Task

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,5 @@
 import PracticeFormPage from "../pageObjects/PracticeForm.page";
+import SortablePage from "../pageObjects/SortablePage";
 
 describe('Demo QA page testing', () => {
   context('practice from test', () => {
@@ -7,22 +8,23 @@ describe('Demo QA page testing', () => {
     });
     //Scenario 1 - ( https://demoqa.com/automation-practice-form ) Forms - Practice forms
     
-   function setMonth(mnth){
-    PracticeFormPage.doBField.click();
-    PracticeFormPage.monthSelectorCalendar.select(mnth);
-    }
+  
 
-    function setYear(yr){
-      PracticeFormPage.doBField.click();
-      PracticeFormPage.yearSelectorCalendar.select(yr);
-      }
-
-    // function setDate(date){ // !!! not working properly, must change selector
-    //     PracticeFormPage.doBField.click();
-    //     PracticeFormPage.dateSelectorCalendar.contains(date).click();
-    //     }
-
-    it('Forms - Practice forms', () => {
+    it.only('Forms - Practice forms', () => {
+      // function setMonth(mnth){ <- couldn't get to work
+      //   PracticeFormPage.doBField.click();
+      //   PracticeFormPage.monthSelectorCalendar.select(mnth);
+      //   };
+    
+      // function setYear(yr){ <- couldn't get to work
+      //     PracticeFormPage.doBField.click();
+      //     PracticeFormPage.yearSelectorCalendar.select(yr);
+      //     };
+    
+      function setDate(x){ 
+            PracticeFormPage.doBField.click();
+            PracticeFormPage.dateSelectorCalendar.contains(x).click();
+            };
       //     - Fill in information
       //     - First Name
       PracticeFormPage.userNameField.click();
@@ -46,24 +48,24 @@ describe('Demo QA page testing', () => {
       //     - Date of birth
       PracticeFormPage.doBField.click();
       //         - Set value - 29/February/1984
+      // - Set date of birth value by using the calendar widget
       let year = '1984'
       let month = 'February'
       let date = '29'
       PracticeFormPage.monthSelectorCalendar.select(month);
       PracticeFormPage.yearSelectorCalendar.select(year);
-      PracticeFormPage.dateSelectorCalendar.contains('29').click();
-      
-      // --------------
-      // setDate('13'); // TO DOOOOOOO
-      //         - Set date of birth value by using the calendar widget
-      //         - Pass the values dynamically, e.g. create methods:
-      //           - setYear(“1984”)
-      // year = '2006';
-      setYear(year);
-      //           - setMonth(“February”)
+      PracticeFormPage.dateSelectorCalendar.contains(date).click();
+      date = '13';
+      setDate(date);
+      // - Pass the values dynamically, e.g. create methods:
+      // - setYear(“1984”) <- couldn't get to work
+      // year = '2006'; 
+      // setYear(year);
+      // - setMonth(“February”) <- couldn't get to work
       // month = 'May'
       // setMonth(month);
-      //           - setDate(“29”)”
+      //     - setDate(“29”)”
+
       //     - Subjects
       //         - Maths
       PracticeFormPage.subjectsField.click();
@@ -73,16 +75,13 @@ describe('Demo QA page testing', () => {
       //     - Current address
       PracticeFormPage.currAddressField.click();
       let address = 'Riga, Latvia'
-      PracticeFormPage.currAddressField.type('Riga, Latvia');
-
-      // ---------
-      //     - State // TO DOOOOOO can't do it on several tries, left for later
+      PracticeFormPage.currAddressField.type(address);
+      //     - State
       PracticeFormPage.stateSelector.click();
-      PracticeFormPage.stateSelector.click();
-      // PracticeFormPage.stateSelector.should('contain.text','NCR').click();
+      PracticeFormPage.stateFirstChoice.click();
       //     - City
-      // PracticeFormPage.citySelector.click(); // TO DOOOO can't do anything till State selected (DOH)
-      // -------
+      PracticeFormPage.citySelector.click();
+      PracticeFormPage.cityFirstChoice.click();
       //  - Click Submit
       PracticeFormPage.submitBtn.click();
       //  - Validate values in the table based on label value
@@ -94,6 +93,33 @@ describe('Demo QA page testing', () => {
       PracticeFormPage.subjectsCheck.should('contain.text','Maths');
       PracticeFormPage.hobbiesCheck.should('contain.text','Reading');
       PracticeFormPage.addressCheck.should('contain.text',address);
+      PracticeFormPage.stateCityCheck.should('contain.text', 'NCR Delhi');
     })
   })
+
+ // Scenario 2( https://demoqa.com/sortable ) Interactions - Sortable
+    context('Interactions - Sortable', () => {
+      beforeEach(() => {
+        cy.visit('https://demoqa.com/sortable')
+      });
+      let values = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+      // - Validate that the values are in order - One, Two, Three, Four, Five, Six
+      // clumsy way but works
+      it('Sortable page testing', () => {
+        SortablePage.firstItem.should('have.text', 'One');
+        SortablePage.secondItem.should('have.text', 'Two');
+        SortablePage.thirdItem.should('have.text', 'Three');
+        SortablePage.forthItem.should('have.text', 'Four');
+        SortablePage.fifthItem.should('have.text', 'Five');
+        SortablePage.sixthItem.should('have.text', 'Six');
+        // - Sort the values in following order - Six, Five, Four, Three, Two, One
+      // - Validate that the values are in order - Six, Five, Four, Three, Two, One
+      })
+    });
+  
+
+ 
+
+
+
 })
